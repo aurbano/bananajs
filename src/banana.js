f276c879ea0b3045a91a96ff1870eb03e108be1c
Alejandro U. Alvarez <aluralma@gmail.com>
2015-10-15 17:13:59 -0400

    Refactor for color palette support

--- src/banana.js
+++ src/banana.js
@@ -10,8 +10,8 @@
 
  var Bananajs = function(options) {
 
-     var Colors = this;
-     Colors.thief = new ColorThief();
+     var Banana = this;
+     Banana.thief = new ColorThief();
 
      if (typeof(options.googleApi) === "undefined") {
          throw {
@@ -21,17 +21,17 @@
 
      var log = function() {
          if (options.debug) console.log.apply(console, arguments);
-     }
+     };
 
      options.googleApi.load("search", "1");
 
      function getImageColor(url) {
          return new Promise(function(resolve) {
              var img = new Image();
-             img.crossOrigin = '';
+             img.crossOrigin = "";
              img.onload = function() {
                  try {
-                     var color = Colors.thief.getColor(img);
+                     var color = Banana.thief.getColor(img, 10);
                      resolve(color);
                  } catch (e) {
                      log("Bananajs: Image download failed");
@@ -45,63 +45,82 @@
          });
      }
 
-     function getImages(colorName, resolve, reject) {
-         // Create an Image Search instance.
-         var imageSearch = new options.googleApi.search.ImageSearch();
+     function getRGBData(colorName) {
+         return new Promise(function(resolve, reject) {
+             // Create an Image Search instance.
+             var imageSearch = new options.googleApi.search.ImageSearch();
 
-         // Set searchComplete as the callback function when a search is 
-         // complete.  The imageSearch object will have results in it.
-         imageSearch.setSearchCompleteCallback(this, function() {
-             var images = imageSearch.results;
-             if (images.length < 1) {
-                 log("Bananajs: No images found");
-                 reject({
-                     message: "No results"
+             // Set searchComplete as the callback function when a search is 
+             // complete.  The imageSearch object will have results in it.
+             imageSearch.setSearchCompleteCallback(this, function() {
+                 var images = imageSearch.results;
+                 if (images.length < 1) {
+                     log("Bananajs: No images found");
+                     reject({
+                         message: "No results"
+                     });
+                 }
+
+                 var promises = [];
+
+                 images.forEach(function(image) {
+                     log("Bananajs: Loading image: ", image.url);
+                     promises.push(getImageColor(image.url));
                  });
-             }
 
-             var promises = [];
-
-             images.forEach(function(image) {
-                 log("Bananajs: Loading image: ", image.url);
-                 promises.push(getImageColor(image.url));
-             });
-
-             Promise.all(promises).then(function(data) {
-                 var r = 0,
-                     g = 0,
-                     b = 0,
-                     count = 0;
-                 data.forEach(function(color) {
-                     if (color !== null) {
-                         count++;
-                         r += color[0];
-                         g += color[1];
-                         b += color[2];
-                     }
+                 Promise.all(promises).then(function(data) {
+                     resolve(data);
                  });
-                 log("Bananajs: Finished processing, color: ", r, g, b);
-                 r /= count;
-                 g /= count;
-                 b /= count;
+             }, null);
 
-                 resolve([r, g, b]);
+             // Find me a beautiful car.
+             imageSearch.execute(colorName);
+         });
+     }
+
+     function getColor(colorName, resolve, reject) {
+         getRGBData(colorName).then(function(data) {
+             var r = 0,
+                 g = 0,
+                 b = 0,
+                 count = 0;
+             data.forEach(function(color) {
+                 if (color !== null) {
+                     count++;
+                     r += color[0];
+                     g += color[1];
+                     b += color[2];
+                 }
              });
-         }, null);
+             log("Bananajs: Finished processing, color: ", r, g, b);
+             r /= count;
+             g /= count;
+             b /= count;
 
-         // Find me a beautiful car.
-         imageSearch.execute(colorName);
+             resolve([r, g, b]);
+         });
      }
 
      // Expose API
-     Colors.getColor = function(colorName) {
-         log("Bananajs: Processing '"+colorName+"'");
+     Banana.getColor = function(colorName) {
+         log("Bananajs: Processing color for '" + colorName + "'");
          return new Promise(
              // The resolver function is called with the ability to resolve or
              // reject the promise
              function(resolve, reject) {
-                 getImages(colorName, resolve, reject);
+                 getColor(colorName, resolve, reject);
              }
          );
      };
+
+     Banana.getPalette = function(colorName) {
+         log("Bananajs: Processing palette for '" + colorName + "'");
+         return new Promise(
+             // The resolver function is called with the ability to resolve or
+             // reject the promise
+             function(resolve, reject) {
+                 getPalette(colorName, resolve, reject);
+             }
+         );
+     }
  };
